5d5d5106c847e687e09008f362bc280c0531e201
Brett <brett.whynot@gmail.com>
2024-10-07 16:02:28 +1000

    progress

--- setup.py
+++ setup.py
@@ -1,29 +1,27 @@
 from setuptools import setup, find_packages
 
-long_description = open('README.md').read()
+long_description = open("README.md").read()
 
 setup(
-    name='pyteslable',
-    version='0.1.4',
-    description='Python interface for connecting to Tesla vehicles directly using the BLE API',
-    readme='README.md',
+    name="tesla_bluetooth",
+    version="0.2.0",
+    description="Python interface for connecting to Tesla vehicles directly using the BLE API",
+    readme="README.md",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/kaedenbrinkman/PyTeslaBLE',
-    author='Kaeden Brinkman',
-    author_email='kaeden@kaedenb.org',
-    license='BSD 2-clause',
-    packages=find_packages(exclude=['test']),
-    install_requires=['simplepyble', 'cryptography', 'protobuf'],
-
+    url="https://github.com/Teslemetry/python-tesla-bluetooth",
+    author="Brett Adams, Kaeden Brinkman, Lex Nastin, Kevin Dewald",
+    author_email="admin@teslemetry.com",
+    license="BSD 2-clause",
+    packages=find_packages(exclude=["test", "example"]),
+    install_requires=["simplepyble", "cryptography", "protobuf"],
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: Microsoft :: Windows :: Windows 10',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.12",
     ],
 )
